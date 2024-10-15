c85fc41a7390de91bbdad6719649a603e2b68ee7
DaniRosales07 <realgraf.ok@gmail.com>
2024-10-15 13:56:24 -0300

    Segunda pre entrega

--- routes/viewsRouter.js
+++ routes/viewsRouter.js
@@ -2,12 +2,11 @@ import { Router } from 'express';
 
 const router = Router();
 
-// Vista principal con la lista de productos
 router.get('/', (req, res) => {
   res.render('home', { title: 'Página Principal', products: [] });
 });
 
-// Vista en tiempo real con productos y formulario para WebSocket
+
 router.get('/realtimeproducts', (req, res) => {
   res.render('realTimeProducts', { title: 'Productos en Tiempo Real' });
 });
